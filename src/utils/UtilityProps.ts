c2c2c1e88ff01439f1e7d6a6ddca29694c4e13d9
LIMPIX31 <limpix31@gmail.com>
2022-07-17 21:03:12 +0200

    UtilityProps update

--- src/utils/UtilityProps.ts
+++ src/utils/UtilityProps.ts
@@ -2,7 +2,7 @@ import { ReactNode } from 'react'
 
 export interface ClassNameable  { className?: string }
 export interface OnClickable { onClick?: (...args: any[]) => any }
-export interface HasChildren { children?: ReactNode }
+export interface HasChildren<T = ReactNode>{ children?: T }
 export interface Valuable<T> { value?: T }
 export interface DataEntriable<T> extends Valuable<T> { onChange?: (newValue: T) => any }
 export interface As { as?: any }
