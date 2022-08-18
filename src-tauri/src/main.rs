5cf5c654283970585c1807b4f3977cb420e06b38
LIMPIX31 <limpix31@gmail.com>
2022-08-18 19:24:41 +0200

    fix gpu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,6 +8,7 @@ pub mod unzip;
 
 #[tokio::main]
 async fn main() {
+  std::env::set_var("WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS", "--ignore-gpu-blocklist");
   let context = tauri::generate_context!();
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![download::download, download::download_abortable, unzip::unzip])
