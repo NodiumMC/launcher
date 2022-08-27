f17a8f084f98e0868874973381a06e558c5f0433
LIMPIX31 <limpix31@gmail.com>
2022-08-27 08:57:13 +0200

    feat: placeholder loading screen

--- src/components/utils/Screen.tsx
+++ src/components/utils/Screen.tsx
@@ -1,6 +1,20 @@
 import styled from 'styled-components'
+import { FC } from 'react'
+import { Preloader } from 'components/micro/Preloader'
 
 export const Screen = styled.div`
   width: 100%;
   height: 100%;
 `
+
+const StyledLoadingScreen = styled(Screen)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const LoadingScreen: FC = () => (
+  <StyledLoadingScreen>
+    <Preloader />
+  </StyledLoadingScreen>
+)
