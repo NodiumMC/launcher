0e9624ca03d90c7b20a2dbb517362ab9dc1a6866
LIMPIX31 <limpix31@gmail.com>
2022-08-30 21:12:48 +0200

    feat: minecraft instance and instance factory

--- src/minecraft/Instance.ts
+++ src/minecraft/Instance.ts
@@ -1,27 +1,62 @@
 import { makeObservable, observable } from 'mobx'
-import type { LauncherProfiles } from 'core/profile/types'
-import { LoggingPool } from 'logging/LoggingPool.service'
+import type { LoggingPool } from 'logging/LoggingPool.service'
+import { autoInjectable } from 'tsyringe'
+import type { Logger } from 'logging'
+import { VersionInstallService } from 'core/services/VersionInstall.service'
+import { Observable } from 'rxjs'
+import { map } from 'utils/map'
+import { launch, LaunchOptions } from 'core'
+import { join } from '@tauri-apps/api/path'
+import { GameDir } from 'native/filesystem'
 
 export class Instance {
   private readonly loggerKey
   @observable name: string
-  @observable readonly launcherProfiles: LauncherProfiles
-  @observable private _isRunning = false
-  @observable private logger
-  @observable selectedProfile: string
+  @observable private currentId?: string
+  @observable private logger: Logger
+  @observable _installed = false
 
   constructor(
     name: string,
-    launcherProfiles: LauncherProfiles,
+    currentId: string,
     private readonly loggingPool: LoggingPool,
-    selectedProfile?: string,
+    private readonly installer: VersionInstallService,
   ) {
     makeObservable(this)
     this.name = name
-    this.launcherProfiles = launcherProfiles
     this.loggerKey = Symbol(name)
-    this.logger = loggingPool.request(this.loggerKey)
-    this.selectedProfile =
-      selectedProfile ?? Object.keys(launcherProfiles.profiles)[0]
+    this.logger = loggingPool!.request(this.loggerKey)
+    this.currentId = currentId
+  }
+
+  get isInstalled() {
+    return this._installed
+  }
+
+  async install() {
+    if(!this.currentId) throw new Error('No version is assigned to this instance')
+    const progress = await this.installer?.install(this.currentId)
+    if(!progress) throw new Error('Installer instance not exists')
+    return new Observable<number>(o => {
+      progress.on('download', data => o.next(map(data.transferred, 0, data.total, 0, 100)))
+      progress.on('unzip', data => o.next(map(data.progress, 0, data.total, 0, -50)))
+      progress.on('done', () => (o.complete(), this._installed = true))
+      progress.on('error', o.error.bind(o))
+    })
+  }
+
+  async launch(options: Omit<LaunchOptions, 'clientDir' | 'gameDir' | 'gameDataDir' | 'vid'>) {
+    if(!this.currentId) throw new Error('No version is assigned to this instance')
+    const command = await launch({
+      ...options,
+      vid: this.currentId,
+      clientDir: await join(await GameDir(), 'versions', this.currentId),
+      gameDir: await join(await GameDir(), 'instances', this.name),
+      gameDataDir: await GameDir()
+    })
+    command.stdout.on('data', this.logger.log.bind(this.logger))
+    command.stderr.on('data', this.logger.log.bind(this.logger))
+    const child = await command.spawn()
+    return this.logger
   }
 }
--- src/minecraft/InstanceFactory.service.ts
+++ src/minecraft/InstanceFactory.service.ts
@@ -1,9 +1,17 @@
-import { singleton } from 'tsyringe'
-import { makeObservable } from 'mobx'
+import { Module } from 'mobmarch'
+import { VersionInstallService } from 'core/services/VersionInstall.service'
+import { LoggingPool } from 'logging'
+import { Instance } from 'minecraft/Instance'
 
-@singleton()
-export class InstancesService {
-  constructor() {
-    makeObservable(this)
+type OmitLast2<T extends any[]> = T extends [ ...infer Head, any, any ] ? Head : any[];
+
+@Module([VersionInstallService, LoggingPool])
+export class InstanceFactory {
+  constructor(
+    private readonly pool: LoggingPool,
+    private readonly installer: VersionInstallService,
+  ) {}
+  new(...args: OmitLast2<ConstructorParameters<typeof Instance>>): Instance {
+    return new Instance(...args, this.pool, this.installer)
   }
 }
