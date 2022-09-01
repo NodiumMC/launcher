e67fd65f24ef12b973ca57be84ae7cd85a60baa0
LIMPIX31 <limpix31@gmail.com>
2022-09-01 10:40:57 +0200

    feat: improve Instance class; Child Process API

--- src/minecraft/Instance.ts
+++ src/minecraft/Instance.ts
@@ -1,6 +1,5 @@
 import { makeObservable, observable } from 'mobx'
 import type { LoggingPool } from 'logging/LoggingPool.service'
-import { autoInjectable } from 'tsyringe'
 import type { Logger } from 'logging'
 import { VersionInstallService } from 'core/services/VersionInstall.service'
 import { Observable } from 'rxjs'
@@ -8,17 +7,19 @@ import { map } from 'utils/map'
 import { launch, LaunchOptions } from 'core'
 import { join } from '@tauri-apps/api/path'
 import { GameDir } from 'native/filesystem'
+import { Child } from '@tauri-apps/api/shell'
 
 export class Instance {
   private readonly loggerKey
   @observable name: string
-  @observable private currentId?: string
-  @observable private logger: Logger
+  @observable private readonly vid?: string
+  @observable private readonly logger: Logger
   @observable _installed = false
+  private child?: Child
 
   constructor(
     name: string,
-    currentId: string,
+    vid: string,
     private readonly loggingPool: LoggingPool,
     private readonly installer: VersionInstallService,
   ) {
@@ -26,7 +27,7 @@ export class Instance {
     this.name = name
     this.loggerKey = Symbol(name)
     this.logger = loggingPool!.request(this.loggerKey)
-    this.currentId = currentId
+    this.vid = vid
   }
 
   get isInstalled() {
@@ -34,9 +35,9 @@ export class Instance {
   }
 
   async install() {
-    if (!this.currentId)
+    if (!this.vid)
       throw new Error('No version is assigned to this instance')
-    const progress = await this.installer?.install(this.currentId)
+    const progress = await this.installer?.install(this.vid)
     if (!progress) throw new Error('Installer instance not exists')
     return new Observable<number>(o => {
       progress.on('download', data =>
@@ -56,18 +57,32 @@ export class Instance {
       'clientDir' | 'gameDir' | 'gameDataDir' | 'vid'
     >,
   ) {
-    if (!this.currentId)
+    if (!this.vid)
       throw new Error('No version is assigned to this instance')
     const command = await launch({
       ...options,
-      vid: this.currentId,
-      clientDir: await join(await GameDir(), 'versions', this.currentId),
+      vid: this.vid,
+      clientDir: await join(await GameDir(), 'versions', this.vid),
       gameDir: await join(await GameDir(), 'instances', this.name),
       gameDataDir: await GameDir(),
     })
     command.stdout.on('data', this.logger.log.bind(this.logger))
     command.stderr.on('data', this.logger.log.bind(this.logger))
-    const child = await command.spawn()
+    this.child = await command.spawn()
     return this.logger
+  }
+
+  async kill() {
+    await this.child?.kill()
+    this.child = undefined
+  }
+
+  get pid(){
+    if(!this.child) throw new Error('Process ID cannot be retrieved until the instance is started')
+    return this.child?.pid
+  }
+
+  get isRunning() {
+    return !!this.child
   }
 }
