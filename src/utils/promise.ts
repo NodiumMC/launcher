2499d19b8babba8a564adee06d4b5808292c911c
LIMPIX31 <limpix31@gmail.com>
2022-09-09 16:35:36 +0200

    feat: promise utils

---
+++ src/utils/promise.ts
@@ -0,0 +1,8 @@
+import { Awaitable } from 'utils/types'
+
+export const isPromise = <T>(
+  possiblePromise: Awaitable<T>,
+): possiblePromise is Promise<T> =>
+  possiblePromise !== undefined &&
+  possiblePromise !== null &&
+  'finally' in possiblePromise
