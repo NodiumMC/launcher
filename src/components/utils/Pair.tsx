b956c6032cd3135c3f5e83ed155614789b58c26a
LIMPIX31 <limpix31@gmail.com>
2022-09-09 21:37:21 +0200

    feat: gap prop

--- src/components/utils/Pair.tsx
+++ src/components/utils/Pair.tsx
@@ -1,7 +1,12 @@
 import styled from 'styled-components'
 
-export const Pair = styled.div`
+export interface PairProps {
+  gap?: 'small' | 'medium' | 'big'
+}
+
+export const Pair = styled.div<PairProps>`
   display: flex;
   align-items: center;
-  gap: 12px;
+  gap: ${({ gap }) =>
+    gap === 'big' ? '24px' : gap === 'small' ? '6px' : '12px'};
 `
