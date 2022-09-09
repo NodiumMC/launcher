5adcb24ce9760c05973c1dc7b5b3c9396597e1d9
LIMPIX31 <limpix31@gmail.com>
2022-09-09 17:40:30 +0200

    feat: create versions subpage

--- src/screens/Main/Main.tsx
+++ src/screens/Main/Main.tsx
@@ -1,10 +1,5 @@
 import { FC } from 'react'
-import {
-  MainScreenPage,
-  MainScreenSidebarSubrouter,
-  MainScreenSubRouter,
-  SubRoute,
-} from './MainScreenSubRouter'
+import { MainScreenPage, MainScreenSidebarSubrouter, MainScreenSubRouter, SubRoute } from './MainScreenSubRouter'
 import { Defer } from 'mobmarch'
 import { Text } from 'components/micro/Text'
 import { PlaySubscreen } from 'screens/Main/PlaySubscreen'
@@ -13,6 +8,7 @@ import { InstanceStore } from 'minecraft/InstanceStore.service'
 import { LoadingScreen } from 'components/utils/Screen'
 import { GameProfileService } from 'core/services/GameProfile.service'
 import { PopupService } from 'notifications'
+import { VersionsSubscreen } from 'screens/Main/VersionsSubscreen'
 
 export const Main: FC = () => {
   return (
@@ -27,6 +23,11 @@ export const Main: FC = () => {
             fallback={<LoadingScreen />}
           >
             <InstancesSubscreen />
+          </Defer>
+        </SubRoute>
+        <SubRoute icon={'download'} to={MainScreenPage.VERSIONS}>
+          <Defer depend={GameProfileService}>
+            <VersionsSubscreen />
           </Defer>
         </SubRoute>
         <SubRoute icon={'terminal'} to={MainScreenPage.CONSOLE}>
---
+++ src/screens/Main/VersionsSubscreen/VersionsSubscreen.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+import styled from 'styled-components'
+import { Screen } from 'components/utils/Screen'
+
+const Page = styled(Screen)``
+
+export const VersionsSubscreen: FC = () => {
+  return <Page></Page>
+}
