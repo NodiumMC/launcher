f33f1f71ef41aaaac176ca6a22f1818f6b30f3e0
LIMPIX31 <limpix31@gmail.com>
2022-09-09 16:41:54 +0200

    feat: improve popup

--- src/components/macro/popup/PopupProps.ts
+++ src/components/macro/popup/PopupProps.ts
@@ -1,14 +1,15 @@
 import { ReactNode } from 'react'
+import { Awaitable } from 'utils/types'
 
 export interface PopupAction {
   label: string
-  action: (close: () => void) => void
+  action: (close: () => void) => Awaitable<void>
   isPrimary?: boolean
   isDanger?: boolean
 }
 
 export interface IPopup {
-  close?: () => void
+  close: () => void
   level: 'ok' | 'warn' | 'error' | 'question' | 'info'
   title: string
   description: ReactNode
