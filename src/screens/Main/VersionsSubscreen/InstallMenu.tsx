fb05905138d76ea5191a61a1b5ea4eaf99159c6a
LIMPIX31 <limpix31@gmail.com>
2022-09-10 22:37:05 +0200

    feat: isnatll menu

--- src/screens/Main/VersionsSubscreen/InstallMenu.tsx
+++ src/screens/Main/VersionsSubscreen/InstallMenu.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react'
+import { FC, useMemo, useState } from 'react'
 import styled from 'styled-components'
 import { Text } from 'components/micro/Text'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -13,6 +13,10 @@ import {
 import { Pair } from 'components/utils/Pair'
 import { Img } from 'components/utils/Img'
 import { useI18N } from 'hooks'
+import useSWR from 'swr'
+import { fetcher } from 'api/swr'
+import { Checkbox } from 'components/micro/Checkbox'
+import { isOld, isRelease } from 'core'
 
 const InstallIcon: FC<Styled> = props => (
   <Text shade={'high'} size={'l'} {...props}>
@@ -36,6 +40,7 @@ const Content = styled.div`
   padding: 6px;
   position: relative;
   display: flex;
+  gap: 24px;
 `
 
 const StyledMenu = styled.div`
@@ -45,22 +50,27 @@ const StyledMenu = styled.div`
   border: 1px solid ${({ theme }) => theme.palette.back.shades[0]};
   border-radius: ${({ theme }) => theme.shape.radius[0]};
   background-color: ${({ theme }) => theme.palette.back.shades[0]};
+
   ${transition('height')}
   &:hover {
     ${StyledInstallIcon} {
       opacity: 0;
     }
+
     ${Content} {
       opacity: 1;
       translate: 0 0;
       scale: 1 1;
     }
+
     height: 100px;
     background-color: ${({ theme }) => theme.palette.back.default};
+
     &:before {
       height: 100px;
     }
   }
+
   &:before {
     content: '';
     display: block;
@@ -96,18 +106,96 @@ const ProviderSelect: FC<DataInput<SupportedProviders>> = ({
   )
 }
 
-const ProviderBox = styled.div`
+const Sqbox = styled.div`
   width: 200px;
   display: flex;
   flex-direction: column;
   gap: 6px;
-  ${Text} {
-    padding: 0 6px;
-  }
+`
+
+interface Version {
+  value: string
+  latest: boolean
+  stable: boolean
+  time: string
+}
+
+const VersionSelect: FC<
+  DataInput<string> & { provider?: SupportedProviders }
+> = ({ provider, onChange, value }) => {
+  const [snapshots, setSnapshots] = useState(false)
+  const [old, setOld] = useState(false)
+  const { data, error } = useSWR<Version[]>(
+    `/${provider}`,
+    fetcher('https://nadmelas.nodium.ru/list'),
+  )
+
+  const options = useMemo(
+    () =>
+      data
+        ?.filter?.(v => snapshots || isRelease(v.value))
+        ?.filter?.(v => old || !isOld(v.value))
+        .map?.(v => ({
+          value: v.value,
+          label: (
+            <Pair>
+              <Text>{v.value}</Text>
+              {v.latest && (
+                <Text color={'#f3ff73'}>
+                  <FontAwesomeIcon icon={'l'} />
+                </Text>
+              )}
+              {!isRelease(v.value) && (
+                <Text color={'#8400ff'}>
+                  <FontAwesomeIcon icon={'s'} />
+                </Text>
+              )}
+              {isOld(v.value) && (
+                <Text color={'#93f5ff'}>
+                  <FontAwesomeIcon icon={'o'} />
+                </Text>
+              )}
+            </Pair>
+          ),
+        })),
+    [value, data, old, snapshots],
+  )
+
+  return (
+    <Sqbox>
+      <Select
+        onChange={onChange}
+        value={value}
+        isLoading={(!data && !!value) || error}
+        options={options}
+        menuPlacement={'top'}
+        placeholder={'Версия'}
+      />
+      <Pair gap={'small'}>
+        <Pair gap={'small'}>
+          <SmallCheckbox value={snapshots} onChange={setSnapshots} />
+          <Text shade={'high'} size={'s'}>
+            Снапшоты
+          </Text>
+        </Pair>
+        <Pair gap={'small'}>
+          <SmallCheckbox value={old} onChange={setOld} />
+          <Text shade={'high'} size={'s'}>
+            Старые
+          </Text>
+        </Pair>
+      </Pair>
+    </Sqbox>
+  )
+}
+
+const SmallCheckbox = styled(Checkbox)`
+  scale: 0.7;
 `
 
 export const InstallMenu: FC = () => {
   const [provider, setProvider] = useState<SupportedProviders | undefined>()
+  const [version, setVersion] = useState<string | undefined>()
 
   const i18n = useI18N()
 
@@ -115,14 +203,19 @@ export const InstallMenu: FC = () => {
     <StyledMenu>
       <StyledInstallIcon />
       <Content>
-        <ProviderBox>
+        <Sqbox>
           <ProviderSelect value={provider} onChange={setProvider} />
           <Text shade={'high'} size={'s'}>
             {provider
               ? i18n.translate.minecraft.providers[provider]
               : 'Выберите поставщика клиента'}
           </Text>
-        </ProviderBox>
+        </Sqbox>
+        <VersionSelect
+          value={version}
+          onChange={setVersion}
+          provider={provider}
+        />
       </Content>
     </StyledMenu>
   )
