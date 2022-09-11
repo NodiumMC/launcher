9f5e6325353cf1f08e822e8062f9767a51a0522e
LIMPIX31 <limpix31@gmail.com>
2022-09-11 09:46:51 +0200

    feat: mojang to vanilla

--- src/core/client-providers/index.ts
+++ src/core/client-providers/index.ts
@@ -1,14 +1,14 @@
-import mojang from 'assets/img/minecraft-mojang.png'
+import vanilla from 'assets/img/minecraft-mojang.png'
 import fabric from 'assets/img/minecraft-fabric.png'
 
 export const VersionProvider = {
-  mojang: 'Mojang',
+  vanilla: 'Vanilla',
   fabric: 'Fabric',
 }
 
 export type SupportedProviders = keyof typeof VersionProvider
 
 export const ProviderIcon: Record<SupportedProviders, string> = {
-  mojang,
+  vanilla,
   fabric,
 }
