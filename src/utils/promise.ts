a115ff2dc9f62f36ec1e5e4b0f52ca6bf36105bc
LIMPIX31 <limpix31@gmail.com>
2022-11-26 21:10:56 +0100

    fix: isPromise

--- src/utils/promise.ts
+++ src/utils/promise.ts
@@ -1,2 +1,2 @@
 export const isPromise = <T>(possiblePromise: Awaitable<T>): possiblePromise is Promise<T> =>
-  possiblePromise !== undefined && possiblePromise !== null && 'finally' in possiblePromise
+  possiblePromise !== undefined && possiblePromise !== null && typeof possiblePromise === 'object' && 'finally' in possiblePromise
