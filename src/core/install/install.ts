3c7c3b98dc39cf1b60255dc723004819bab7096f
LIMPIX31 <limpix31@gmail.com>
2022-11-28 12:47:00 +0100

    feat: log4j config

--- src/core/install/install.ts
+++ src/core/install/install.ts
@@ -1,9 +1,11 @@
 import { compileAssetIndex, compileLibraries, readVersionFile } from 'core'
 import { join } from 'native/path'
+import { log4jConfig } from 'core/log4j'
 
 export const compileLocal = async (vid: string, clientDir: string, gameDataDir: string) => {
   const version = await readVersionFile(join(clientDir, `${vid}.json`))
   const libs = compileLibraries(version.libraries, gameDataDir, clientDir)
   const assets = await compileAssetIndex(version, gameDataDir)
-  return [...libs, ...assets, { ...version.downloads.client, local: join(clientDir, `${vid}.jar`) }]
+  const logging = await log4jConfig(version, clientDir)
+  return [...libs, ...assets, logging, { ...version.downloads.client, local: join(clientDir, `${vid}.jar`) }]
 }
