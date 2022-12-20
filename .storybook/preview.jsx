1f240be7c83ae8797ffa7e28722779b0a171e849
Danil Karpenko <81296950+LIMPIX31@users.noreply.github.com>
2022-12-20 19:58:08 +0100

    feat: setup storybook (#211)

---
+++ .storybook/preview.jsx
@@ -0,0 +1,24 @@
+import theme from './theme'
+import { ThemeProvider } from 'styled-components'
+import { dark } from '../src/theme/list/dark'
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  docs: {
+    theme
+  }
+}
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={dark}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
