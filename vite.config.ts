a057e311d22cda90e374fd0d4e5b2904be9643b2
Danil Karpenko <81296950+LIMPIX31@users.noreply.github.com>
2023-01-02 20:35:29 +0100

    feat: improve vite config (#243)

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,15 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), swc.vite()],
+  clearScreen: false,
   server: {
     port: 3000,
+    strictPort: true,
   },
-  resolve: {
-    alias: {
-      path: 'path-browserify',
-    },
+  envPrefix: ['VITE_', 'TAURI_'],
+  build: {
+    target: ['es2021', 'chrome100', 'safari13'],
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    sourcemap: !!process.env.TAURI_DEBUG,
   },
 })
