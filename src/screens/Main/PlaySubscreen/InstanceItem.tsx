04c5a5acea3415d642c76b4bff5464e22190e249
LIMPIX31 <limpix31@gmail.com>
2023-01-04 15:19:41 +0100

    refactor: instance class

--- src/screens/Main/PlaySubscreen/InstanceItem.tsx
+++ src/screens/Main/PlaySubscreen/InstanceItem.tsx
@@ -68,7 +68,6 @@ const Progress = styled.svg<{ stage: number }>`
     stroke: ${({ theme, stage }) =>
       stage === 0 ? theme.palette.orange : stage === 1 ? theme.palette.yellow : theme.accent.primary};
     stroke-dasharray: 133;
-    stroke-dashoffset: 133;
     ${transition('stroke-dashoffset')}
   }
 `
@@ -93,34 +92,23 @@ const HideableButton = styled(Button)<{ hide: boolean }>`
 `
 
 export const InstanceItem: FC<InstanceItemProps> = observer(({ instance }) => {
-  const progress = useRef<SVGPathElement | null>(null)
-  const [stage, setStage] = useState(0)
   const upfall = useMod(UpfallService)
   const popup = useMod(PopupService)
   const i18n = useI18N(t => t.minecraft.instance)
 
-  const setProgress = useCallback(
-    (value: number) => {
-      if (!progress.current) return
-      progress.current.style.strokeDashoffset = value.map(0, 100, 133, 0).toString()
-    },
-    [progress],
-  )
-
   const launch = useCallback(() => {
-    instance.launch().subscribe({
-      error(code) {
-        const lastEvent = instance.logs.last
-        if ((typeof code === 'number' && code !== 0) || lastEvent?.throwable)
-          popup.create(Popup, {
-            title: i18n.minecraft_crashed,
-            description: (lastEvent?.message ?? '') + '\n\n' + (lastEvent?.throwable ?? ''),
-            level: 'error',
-            actions: [{ label: 'Ок', isPrimary: true, action: close => close() }],
-          })
-        else if (typeof code === 'string' || typeof code === 'object')
-          upfall.drop('error', t => `${t.minecraft.instance.launch_failed}: ${erm(code)}`)
-      },
+    instance.launch().catch(code => {
+      console.log(code)
+      const lastEvent = instance.logs.last
+      if ((typeof code === 'number' && code !== 0) || (typeof code === 'number' && lastEvent?.throwable))
+        popup.create(Popup, {
+          title: i18n.minecraft_crashed,
+          description: (lastEvent?.message ?? '') + '\n\n' + (lastEvent?.throwable ?? ''),
+          level: 'error',
+          actions: [{ label: 'Ок', isPrimary: true, action: close => close() }],
+        })
+      else if (typeof code === 'string' || typeof code === 'object')
+        upfall.drop('error', t => `${t.minecraft.instance.launch_failed}: ${erm(code)}`)
     })
   }, [])
 
@@ -131,26 +119,15 @@ export const InstanceItem: FC<InstanceItemProps> = observer(({ instance }) => {
   const handle = useCallback(() => {
     if (instance.child) kill()
     else if (!instance.isInstalled) {
-      instance.install().subscribe({
-        next(value) {
-          if (value.stage > stage) {
-            setStage(value.stage)
-            setProgress(value.progress)
-          }
-          if (value.stage === stage) setProgress(value.progress)
-        },
-        error(err) {
+      instance
+        .install()
+        .catch(err => {
           if (err?.startsWith?.('Network Error')) upfall.drop('error', t => t.minecraft.instance.network_error)
           else upfall.drop('error', t => t.minecraft.instance.install_failed)
-          setProgress(0)
-        },
-        complete() {
-          setProgress(0)
-          launch()
-        },
-      })
+        })
+        .then(launch)
     } else launch()
-  }, [setProgress])
+  }, [])
 
   const settings = useCallback(() => {
     popup.create(InstanceEditor, { instance })
@@ -194,10 +171,9 @@ export const InstanceItem: FC<InstanceItemProps> = observer(({ instance }) => {
         version="1.1"
         viewBox="0 0 38 38"
         xmlns="http://www.w3.org/2000/svg"
-        stage={stage}
+        stage={instance.progress.stage ?? 0}
       >
         <path
-          ref={progress}
           x={1}
           y={1}
           width={36}
@@ -205,6 +181,7 @@ export const InstanceItem: FC<InstanceItemProps> = observer(({ instance }) => {
           d={'m7 1h24a6 6 45 0 1 6 6v24a6 6 135 0 1-6 6h-24a6 6 45 0 1-6-6v-24a6 6 135 0 1 6-6z'}
           fill={'none'}
           strokeWidth={'2'}
+          strokeDashoffset={instance.progress.progress.map(0, 100, 133 ,0)}
         />
       </Progress>
     </Container>
