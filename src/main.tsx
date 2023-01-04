357f258fecc21499fad5c91df3c60d75fa78dfbc
LIMPIX31 <limpix31@gmail.com>
2023-01-04 16:49:38 +0100

    feat: implement deeplink open path

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,19 @@ import { App } from 'App'
 import { MemoryRouter } from 'react-router-dom'
 import { time } from 'debug'
 import { configure } from 'mobx'
+import { register } from 'native/deeplink'
+import { appWindow } from '@tauri-apps/api/window'
 
 configure({
   enforceActions: 'never',
 })
 
 time('Startup', 'startup')
+
+register('open', () => {
+  appWindow.unminimize()
+  appWindow.setFocus()
+})
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
