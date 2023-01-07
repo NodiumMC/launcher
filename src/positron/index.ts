ee5fca0c4c7aee886da00377a472a6f9bfdc1c58
LIMPIX31 <limpix31@gmail.com>
2023-01-07 17:18:31 +0100

    feat: update positron

--- src/positron/index.ts
+++ src/positron/index.ts
@@ -1,4 +1,6 @@
-import { singleton } from 'tsyringe'
+import { autoInjectable, singleton } from '@nodium/tsyringe'
 
 export const Service: ClassDecorator = target => singleton()(target as any)
 export const Module: ClassDecorator = target => singleton()(target as any)
+export const DynamicModule: ClassDecorator = target => autoInjectable()(target as any)
+export const DynamicService: ClassDecorator = target => autoInjectable()(target as any)
