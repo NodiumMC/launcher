6158f709d7f96b21e965f3cb2c72579d04e18e50
LIMPIX31 <limpix31@gmail.com>
2023-01-07 17:19:59 +0100

    feat: new Promise() shortcut

--- src/utils/promise.ts
+++ src/utils/promise.ts
@@ -3,3 +3,7 @@ export const isPromise = <T>(possiblePromise: Awaitable<T>): possiblePromise is 
   possiblePromise !== null &&
   typeof possiblePromise === 'object' &&
   'finally' in possiblePromise
+
+export const promise = <T = void>(
+  executor: (resolve: (value: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void,
+) => new Promise(executor)
