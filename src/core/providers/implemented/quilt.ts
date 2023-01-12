24217d4d046ffcb28b8da4d73ab755a465b1e2e8
LIMPIX31 <limpix31@gmail.com>
2023-01-12 09:43:58 +0100

    fix: quilt provider

--- src/core/providers/implemented/quilt.ts
+++ src/core/providers/implemented/quilt.ts
@@ -1,4 +1,4 @@
-import { mergeVersions, VersionFile } from 'core/version'
+import { VersionFile } from 'core/version'
 import { fetchQuiltLoaders, fetchQuiltManifest, Provider } from '..'
 import { quiltVersion } from '../endpoints'
 import { fetch } from '@tauri-apps/api/http'
@@ -7,8 +7,7 @@ export const quilt: Provider = async (id, loader?: string) => {
   const quiltManifest = await fetchQuiltManifest()
   const quiltVersions = quiltManifest.map(v => v.version)
   if (!quiltVersions.some(v => v === id)) throw new Error(`Version ${id} isn't supported by Quilt mod loader`)
-  const quilt = await fetch<VersionFile>(
+  return fetch<VersionFile>(
     quiltVersion.explain({ id, loader: loader ?? (await fetchQuiltLoaders(id).then(v => v[0].loader.version)) }),
   ).then(v => v.data)
-  return mergeVersions(quilt)
 }
