ef4d635931fe6784d77437fc2fb83d155bf3de8c
LIMPIX31 <limpix31@gmail.com>
2023-03-29 07:43:50 +0200

    feat: types for useTheme

--- shared/styled/src/index.ts
+++ shared/styled/src/index.ts
@@ -1,3 +1,6 @@
+import { Schema } from '@theme/types'
+import { useTheme as useEmotionTheme } from '@emotion/react'
+
 export * from '@emotion/styled'
 export * from '@emotion/react'
 export { cx } from '@emotion/css'
@@ -9,3 +12,7 @@ export * from './prop-tools'
 export * from './style-fn'
 
 export { default as styled } from '@emotion/styled'
+
+export function useTheme() {
+  return useEmotionTheme() as Schema
+}
