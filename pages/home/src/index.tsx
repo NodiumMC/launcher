0e806ba317cedb713c21ddb523186c4a6c5ca63e
LIMPIX31 <limpix31@gmail.com>
2023-04-10 08:44:12 +0200

    chore(pages): remove home page layout

--- pages/home/src/index.tsx
+++ pages/home/src/index.tsx
@@ -1,14 +1,3 @@
 import { FC } from 'react'
-import { Box } from '@ui/layout'
-import { useTheme } from 'styled'
-import { HomeLayout } from './home-layout.component'
 
-const Placeholder: FC = props => {
-  const theme = useTheme()
-
-  return <Box backgroundColor={theme.palette.gray._50} {...props} />
-}
-
-export const HomePage: FC = () => (
-  <HomeLayout User={Placeholder} Skin={Placeholder} Main={Placeholder} Play={Placeholder} Options={Placeholder} />
-)
+export const HomePage: FC = () => <div />
