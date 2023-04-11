e42fe53aeca7be4c045848e68ecdbedfac9c0130
LIMPIX31 <limpix31@gmail.com>
2023-04-11 17:59:07 +0200

    fix: icon names

--- shared/ui/icons/src/fontawesome.tsx
+++ shared/ui/icons/src/fontawesome.tsx
@@ -1,11 +1,17 @@
 import { FC } from 'react'
 import { FontAwesomeIcon, FontAwesomeIconProps } from '@fortawesome/react-fontawesome'
-import { faMinus as solidMinus, faXmark as solidXmark, faHouse } from '@fortawesome/free-solid-svg-icons'
+import {
+  faMinus as solidMinus,
+  faXmark as solidXmark,
+  faBoltLightning,
+  faDownload,
+} from '@fortawesome/free-solid-svg-icons'
 import { faSquare as regularSquare } from '@fortawesome/free-regular-svg-icons'
 
 type IconFC = FC<Omit<FontAwesomeIconProps, 'icon'>>
 
-export const CloseWindowIcon: IconFC = props => <FontAwesomeIcon {...props} icon={solidXmark} />
-export const MinimizeWindowIcon: IconFC = props => <FontAwesomeIcon {...props} icon={solidMinus} />
-export const MaximizeWindowIcon: IconFC = props => <FontAwesomeIcon {...props} icon={regularSquare} />
-export const HomeIcon: IconFC = props => <FontAwesomeIcon {...props} icon={faHouse} />
+export const FaSolidXMarkIcon: IconFC = props => <FontAwesomeIcon {...props} icon={solidXmark} />
+export const FaSolidMinusIcon: IconFC = props => <FontAwesomeIcon {...props} icon={solidMinus} />
+export const FaRegularSquareIcon: IconFC = props => <FontAwesomeIcon {...props} icon={regularSquare} />
+export const FaSolidBoltLightningIcon: IconFC = props => <FontAwesomeIcon {...props} icon={faBoltLightning} />
+export const FaSolidDownloadIcon: IconFC = props => <FontAwesomeIcon {...props} icon={faDownload} />
