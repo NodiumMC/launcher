29485e45c08333f9026c8ce2bf29f5e781986a92
Danil Karpenko <limpix31@gmail.com>
2023-06-02 17:41:21 +0200

    fix: typecheck

--- shared/ui/window/src/bordered/bordered.styles.ts
+++ shared/ui/window/src/bordered/bordered.styles.ts
@@ -2,7 +2,7 @@ import { type StyleFn } from '@lmpx/styled'
 import { BorderedProps } from './bordered.interface'
 
 
-export const styles: StyleFn<BorderedProps> = ({ sides, theme }) => ({
+export const styles: StyleFn<BorderedProps> = ({ sides = [], theme }) => ({
   borderColor: theme.palette.gray._50,
   borderTopWidth: sides.includes('top') ? '1px' : '0',
   borderBottomWidth: sides.includes('bottom') ? '1px' : '0',
